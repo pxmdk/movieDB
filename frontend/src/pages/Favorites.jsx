72f244d699ca39918c5732d9fcd6f47c26ce5c3e
Jacob Andersen <jacob@pixelmonkey.dk>
2025-03-07 09:45:05 +0100

    Added Page Routing to pages

---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+function Favorites() {
+  return (
+    <div className="favorites-empty">
+      <h2>No favorites movies yet</h2>
+      <p>Start adding movies to your favorites list</p>
+    </div>
+  );
+}
+
+export default Favorites;
